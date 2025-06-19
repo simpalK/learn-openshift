557d0101804dded1eceaadb3b946bd252ccd89c4
simpalK <simpal.kumar@gmail.com>
2025-06-19 22:03:02 +0200

    hello world

---
+++ hello.py
@@ -0,0 +1,10 @@
+# hello.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, OpenShift!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
